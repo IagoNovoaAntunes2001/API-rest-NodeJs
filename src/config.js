a6de013c2f77c46944c393a82e2db48d4b7d85b3
Iago Novoa Antunes <inovoaan@everis.com>
2020-06-15 14:22:23 -0300

    Conexao com o Mlab no heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 
 module.exports = {
-    connectionStringToHeroku: 'mongodb://Iago:novoa@ds121345.mlab.com:21345/heroku_6d83w007',
+    connectionStringToHeroku: 'mongodb://Iago:novoa123@ds121345.mlab.com:21345/heroku_6d83w007',
     connectionString: 'mongodb+srv://Iago:novoa@nodestr-4z5jj.azure.mongodb.net/test?retryWrites=true&w=majority',
     containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=noderestapi;AccountKey=rm0aEcsOY+o5PPGmrt/t02YqyoHzcm7NmMOHZnBBPadw9CXfEbDKTbaT/N0qD0ItzWJqDzEsRDob6jtp7W8C4Q==;EndpointSuffix=core.windows.net'
 }
